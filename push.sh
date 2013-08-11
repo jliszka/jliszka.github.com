3da124826d487e61d48c2a70e28034744ccc2997
Jason Liszka <jliszka@alumni.cmu.edu>
2013-08-11 13:02:44 -0400

    Site updated

--- push.sh
+++ push.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-FILES=$(\ls -1 .)
-
 git checkout master && \
-for f in $FILES; do git rm -r $f; done && \
+for f in $(\ls -1 .); do git rm -r $f; done && \
 cp -a _site/* . && \
 git add . && \
 git commit -m 'Site updated' && \
